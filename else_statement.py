66240750f766d8c3fa41e7ef660f33665e0a8fd3
surbhiyadav6 <surbhiy1920@gmail.com>
2026-03-23 03:02:01 -0400

    adding

--- else_statement.py
+++
@@ -1,8 +0,0 @@
-## the else statement is executes the block of code if the condition of "if statement" is not true
-
-age = 17
-
-if age>=18:
-    print("you are an adult")
-else:
-    print("you are a teenager")
--- if_statement.py
+++
@@ -1,6 +0,0 @@
-## if statement
-
-age = 20
-
-if age>=18:
-    print("You are eligible to vote")
