aa0eab1e6e4c6f59af9a51581b5be6b93c765cf3
abhinav-1067 <abhinavyadav1673@gmail.com>
2026-08-23 05:27:25 -0400

    ipl api

--- API_MAKING/ipl-api-service/app.py
+++ API_MAKING/ipl-api-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, request
 from ipl import *
 
 app = Flask('__name__')
@@ -12,11 +12,14 @@ def teams():
     func = teamsAPI()
     return jsonify(func)
 
-@app.route('/api/teams_vs_teams')
+@app.route('/api/teamvteam')
 def teams_vs_teams():
-    func = teams_vs_teamsAPI()
-    t = func('Royal Challengers Bangalore','Lucknow Super Giants')
-    return jsonify(t)
+   
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+    func = teams_vs_teamsAPI(team1,team2)
+    
+    return jsonify(func)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- API_MAKING/ipl-api-service/ipl.py
+++ API_MAKING/ipl-api-service/ipl.py
@@ -12,22 +12,22 @@ def teamsAPI():
     }
     return teams_dict
 
-def teams_vs_teamsAPI():
-    team1 = 'Royal Challengers Bangalore'
-    team2 = 'Sunrisers Hyderabad'
+def teams_vs_teamsAPI(team1,team2):
+    condition = ( ((ipl_matches['Team1'] == team1) & (ipl_matches['Team2']==team2)) | ((ipl_matches['Team1'] == team2) & (ipl_matches['Team2']==team1))
 
-    matches_played = (ipl_matches['Team1'] == team1) & (ipl_matches['Team2']==team2) | (ipl_matches['Team1'] == team2) & (ipl_matches['Team2']==team1)
+    )
+    matches_played = ipl_matches[condition]
     total_matches = matches_played.shape[0]
 
-    match_won_team1 = matches_played['WinningTeam'].value_counts()[team1]
-    match_won_team2 = matches_played['WinningTeam'].value_counts()[team2]
+    match_won_team1 = matches_played['WinningTeam'].value_counts().get(team1,0)
+    match_won_team2 = matches_played['WinningTeam'].value_counts().get(team2,0)
 
     drawn = total_matches - (match_won_team1 + match_won_team2)
 
     response = {
-        "Number of Matches played": total_matches,
-        "Matches Won by Team1": match_won_team1,
-        "Matches Won by Team2": match_won_team2,
-        "Matches Drawn": drawn
+        "Number of Matches played": int(total_matches),
+        f"Matches Won By {team1}": int(match_won_team1),
+        f"Matches Won By {team2}": int(match_won_team2),
+        "Matches Drawn": int(drawn)
     }
     return response
