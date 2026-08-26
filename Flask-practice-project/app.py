c2e794b3e1a96145b72d4357b9fd070ef71d01dd
abhinav-1067 <abhinavyadav1673@gmail.com>
2026-08-26 06:26:07 -0400

    Pandas GroupBy

--- Flask-practice-project/app.py
+++ Flask-practice-project/app.py
@@ -1,42 +1,7 @@
-from flask import Flask, render_template,request
-from db import Database
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-dbo = Database()
-
 @app.route("/")
 def index():
-    return render_template('login.html')
-
-@app.route("/register")
-def register():
-    return render_template('register.html')
-
-@app.route("/perform_registration", methods=['post'])
-def perform_registration():
-    name = request.form.get('users_name')
-    email = request.form.get('users_email')
-    password = request.form.get('users_password')
-
-    response = dbo.insert(name,email,password)
-    if response:
-        return render_template('login.html',message="Registered Successsfully! Kindly Login Here")
-    else:
-        return render_template('register.html', message="Email Already Exists!")
-
-@app.route("/perform_login", methods=["post"])
-def perform_login():
-    email = request.form.get("user_email" )
-    password = request.form.get("user_password")
-
-    response = dbo.search(email,password)
-
-    if response==1:
-        return "Login Successfully!"
-    else:
-        return render_template("login.html", message="Login Failed!")
-
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("login.html")
